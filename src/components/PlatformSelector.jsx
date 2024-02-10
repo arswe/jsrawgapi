d9cf3330726f6a630f0e3e84ab294f030917a313
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-10 23:44:03 +0600

    PlatformSelector

--- src/components/PlatformSelector.jsx
+++ src/components/PlatformSelector.jsx
@@ -1,5 +1,34 @@
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import { useState } from 'react'
+
 const PlatformSelector = () => {
-  return <div>PlatformSelector</div>
+  const [age, setAge] = useState('')
+
+  const handleChange = (event) => {
+    setAge(event.target.value)
+  }
+
+  return (
+    <Box sx={{ minWidth: 160 }}>
+      <FormControl fullWidth>
+        <InputLabel size='small' id='PlatformSelector'>
+          Age
+        </InputLabel>
+        <Select
+          labelId='PlatformSelector'
+          id='demo-simple-select'
+          value={age}
+          label='Age'
+          size='small'
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  )
 }
 
 export default PlatformSelector
