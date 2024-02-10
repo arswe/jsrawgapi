f9f5ffa5fed301b309eb8fdde9a172fc7b0e1eda
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-10 23:24:48 +0600

    side effect remove

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -7,25 +7,23 @@ const useData = (endpoint, requestConfig, deps) => {
   const [error, getError] = useState('')
   const [isLaoding, setIsLaoding] = useState(false)
 
-  useEffect(
-    () => {
-      const controller = new AbortController()
-      setIsLaoding(true)
-      apiClient
-        .get(endpoint, { signal: controller.signal, ...requestConfig })
-        .then((response) => {
-          setData(response.data.results)
-          setIsLaoding(false)
-        })
-        .catch((err) => {
-          if (err instanceof CanceledError) return
-          getError(err.message)
-          setIsLaoding(false)
-        })
-      return () => controller.abort()
-    },
-    deps ? [...deps] : ''
-  )
+  useEffect(() => {
+    const controller = new AbortController()
+    setIsLaoding(true)
+    apiClient
+      .get(endpoint, { signal: controller.signal, ...requestConfig })
+      .then((response) => {
+        setData(response.data.results)
+        setIsLaoding(false)
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return
+        getError(err.message)
+        setIsLaoding(false)
+      })
+    return () => controller.abort()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps || [])
 
   return { data, error, isLaoding }
 }
