d83b7eefa32aca91f5a08c9d3dce8c9d17f8403a
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-11 22:43:36 +0600

    useGenres

--- src/hooks/useGenres.js
+++ src/hooks/useGenres.js
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
+import apiClient from '../services/api-client'
 
 const useGenres = () =>
   useQuery({
-    queryKey: ['genre'],
-    queryFn: () => ,
+    queryKey: ['genres'],
+    queryFn: () => apiClient.get('/genres').then((res) => res.data),
   })
 
 export default useGenres
