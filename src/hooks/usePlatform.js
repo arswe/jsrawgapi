0d5001a2d39e27dc3e437c1569a50d466cd09abc
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-11 22:51:50 +0600

    usePlatform hooks

--- src/hooks/usePlatform.js
+++ src/hooks/usePlatform.js
@@ -1,5 +1,13 @@
+import { useQuery } from '@tanstack/react-query'
 import platforms from '../data/platforms'
+import apiClient from '../services/api-client'
 
-const usePlatform = () => ({ data: platforms, isLoading: false, error: null })
+const usePlatform = () =>
+  useQuery({
+    queryKey: ['platforms'],
+    queryFn: () => apiClient.get('/platforms').then((res) => res.data),
+    staleTime: 10 * 60 * 60 * 1000,
+    initialData: { count: platforms.length, results: platforms },
+  })
 
 export default usePlatform
