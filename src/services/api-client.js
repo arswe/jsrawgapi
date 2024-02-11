a17c22ab30b2e2ab4eea1b04b921cc15a4045da9
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-11 23:08:33 +0600

    APIClient

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -1,10 +1,20 @@
 import axios from 'axios'
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: '3ed92c68725a419998140fb0bf99f728',
   },
 })
 
+class APIClient {
+  constructor(endpoint) {
+    this.endpoint = endpoint
+  }
 
+  getAll(config) {
+    return axiosInstance.get(this.endpoint, config).then((res) => res.data)
+  }
+}
+
+export default APIClient
